ca42d2cc6e9ba1763c1b0b0ca6557d8091721ba7
HAMZA-CH <hakram554r@gmail.com>
2025-06-26 21:13:34 +0500

    minor changes

--- lib/Features/AuthenticationScreens/SignUpScreens/address_screen.dart
+++ lib/Features/AuthenticationScreens/SignUpScreens/address_screen.dart
@@ -1,3 +1,4 @@
+import 'package:acaste/Features/AuthenticationScreens/SignUpScreens/user_data_screen.dart';
 import 'package:acaste/Features/AuthenticationScreens/widgets/custom_text_field.dart';
 import 'package:acaste/Features/AuthenticationScreens/widgets/sign_button.dart';
 import 'package:flutter/material.dart';
@@ -12,45 +13,55 @@ class AddressScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            spacing: 35,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  "assets/logo/logo2.png",
-                  height: 115,
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 35,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/logo/logo2.png",
+                    height: 115,
+                  ),
                 ),
-              ),
-              Text(
-                "Create Account",
-                style: GoogleFonts.poppins(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+                Text(
+                  "Create Account",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Column(
-                spacing: 8,
-                children: [
-                  CustomTextField(
-                      hintText: "Street Address",
-                      icon: Icons.location_on_outlined),
-                  CustomTextField(hintText: "Zip Code", icon: Icons.newspaper),
-                  CustomTextField(hintText: "State", icon: Icons.location_city),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.sizeOf(context).height / 6,
-              ),
-              SignButton(
-                btnName: "Next",
-                onTap: () {},
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  spacing: 8,
+                  children: [
+                    CustomTextField(
+                        hintText: "Street Address",
+                        icon: Icons.location_on_outlined),
+                    CustomTextField(
+                        hintText: "Zip Code", icon: Icons.newspaper),
+                    CustomTextField(
+                        hintText: "State", icon: Icons.location_city),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 6,
+                ),
+                SignButton(
+                  btnName: "Next",
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserDataScreen(),
+                        ));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Features/AuthenticationScreens/SignUpScreens/user_data_screen.dart
+++ lib/Features/AuthenticationScreens/SignUpScreens/user_data_screen.dart
@@ -1,7 +1,9 @@
 import 'package:acaste/Features/AuthenticationScreens/widgets/custom_text_field.dart';
 import 'package:acaste/Features/AuthenticationScreens/widgets/sign_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
 
 class UserDataScreen extends StatelessWidget {
   const UserDataScreen({super.key});
@@ -12,45 +14,62 @@ class UserDataScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            spacing: 35,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  "assets/logo/logo2.png",
-                  height: 115,
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 35,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/logo/logo2.png",
+                    height: 115,
+                  ),
                 ),
-              ),
-              Text(
-                "Create Account",
-                style: GoogleFonts.poppins(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+                Text(
+                  "Create Account",
+                  style: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Column(
-                spacing: 8,
-                children: [
-                  CustomTextField(
-                      hintText: "Street Address",
-                      icon: Icons.location_on_outlined),
-                  CustomTextField(hintText: "Zip Code", icon: Icons.newspaper),
-                  CustomTextField(hintText: "State", icon: Icons.location_city),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.sizeOf(context).height / 6,
-              ),
-              SignButton(
-                btnName: "Next",
-                onTap: () {},
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  spacing: 8,
+                  children: [
+                    CustomTextField(
+                      hintText: "First Name",
+                      icon: CupertinoIcons.person_crop_circle,
+                    ),
+                    CustomTextField(
+                      hintText: "Last Name",
+                      icon: CupertinoIcons.person_crop_circle,
+                    ),
+                    CustomTextField(
+                      hintText: "Email",
+                      icon: CupertinoIcons.mail,
+                    ),
+                    CustomTextField(
+                      hintText: "Password",
+                      icon: Iconsax.password_check,
+                    ),
+                    CustomTextField(
+                      hintText: "Confirm Password",
+                      icon: Iconsax.password_check,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 17,
+                ),
+                SignButton(
+                  btnName: "Next",
+                  onTap: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
