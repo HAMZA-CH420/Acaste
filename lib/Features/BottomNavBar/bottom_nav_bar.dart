62e1daf25f64306f407da58b79fefae953fc92b2
HAMZA-CH <hakram554r@gmail.com>
2025-06-27 21:43:17 +0500

    minor changes

---
+++ lib/Features/BottomNavBar/bottom_nav_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: []),
+    );
+  }
+}
