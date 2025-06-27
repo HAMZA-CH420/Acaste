ff01052e2f9dd6afc5d9c7d0967e5939e6b6c346
HAMZA-CH <hakram554r@gmail.com>
2025-06-27 22:06:35 +0500

    minor changes

--- lib/Features/BottomNavBar/bottom_nav_bar.dart
+++ lib/Features/BottomNavBar/bottom_nav_bar.dart
@@ -1,23 +1,39 @@
 import 'package:acaste/Features/UiHelpers/ColorPalette/color_palette.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
   @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(),
       bottomNavigationBar: BottomNavigationBar(
-        iconSize: 28,
+        currentIndex: currentIndex,
+        elevation: 0,
+        iconSize: 30,
         type: BottomNavigationBarType.fixed,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         selectedItemColor: Palette.primaryColor,
         unselectedItemColor: Colors.grey.shade400,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Iconsax.home), label: "home"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today_outlined), label: "calender"),
+              icon: Icon(Iconsax.calendar), label: "calender"),
+          BottomNavigationBarItem(icon: Icon(Iconsax.document), label: "blog"),
+          BottomNavigationBarItem(
+              icon: Icon(Iconsax.notification), label: "Notification"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person_crop_circle), label: "profile"),
         ],
       ),
     );
