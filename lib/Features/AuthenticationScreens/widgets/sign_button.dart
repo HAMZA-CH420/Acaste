a8a649c55fd887e165b6f5964cfb24885bfa14b7
HAMZA-CH <hakram554r@gmail.com>
2025-06-28 22:29:07 +0500

    minor changes

--- lib/Features/AuthenticationScreens/widgets/sign_button.dart
+++ lib/Features/AuthenticationScreens/widgets/sign_button.dart
@@ -1,4 +1,4 @@
-import 'package:acaste/Features/UiHelpers/ColorPalette/color_palette.dart';
+import 'package:acaste/UiHelpers/ColorPalette/color_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
