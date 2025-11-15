88a38198126aebb6c1a8e68429edbdd7c8913a01
Hamza <hakram554r@gmail.com>
2025-11-15 11:38:28 +0500

    minor changes

--- lib/Features/BlogScreen/widgets/updates.dart
+++ lib/Features/BlogScreen/widgets/updates.dart
@@ -27,7 +27,7 @@ class _UpdatesState extends State<Updates> {
       "authorName": "UsmanCh",
       "title": "Rising Talent",
       "subtitle": "Apple Watch Series 5: Finally, always on",
-      "titleBackground": "blogImage2.jpg"
+      "titleBackground": "blogImage3.jpg"
     },
   ];
 
@@ -37,78 +37,93 @@ class _UpdatesState extends State<Updates> {
     return SizedBox(
       height: size.height / 4.4,
       width: size.width,
-      child: PageView(
-        children: [
-          updateWidget(Colors.blue, size),
-          updateWidget(Colors.amber, size),
-        ],
+      child: PageView.builder(
+        itemCount: updates.length,
+        itemBuilder: (context, index) {
+          return updateWidget(
+            image: updates[index]['titleBackground'],
+            title: updates[index]['title'],
+            subtitle: updates[index]['subtitle'],
+            authorName: updates[index]['authorName'],
+            boxSize: size,
+          );
+        },
       ),
     );
   }
 
   /// Blog widget to display
-  Widget updateWidget(Color color, var boxSize) {
-    return Container(
-      margin: EdgeInsets.only(right: 15),
-      padding: EdgeInsets.symmetric(horizontal: 22, vertical: 15),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 15,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Container(
-                height: boxSize.height / 20,
-                width: boxSize.width / 6.5,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD34D69),
-                  borderRadius: BorderRadius.circular(17),
+  Widget updateWidget({
+    required String image,
+    required String title,
+    required String subtitle,
+    required String authorName,
+    required var boxSize,
+  }) {
+    return Stack(children: [
+      Container(
+        margin: EdgeInsets.only(right: 15),
+        padding: EdgeInsets.symmetric(horizontal: 22, vertical: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+              image: AssetImage("assets/images/$image"), fit: BoxFit.cover),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 15,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  height: boxSize.height / 20,
+                  width: boxSize.width / 6.5,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD34D69),
+                    borderRadius: BorderRadius.circular(17),
+                  ),
+                  child: Icon(
+                    CupertinoIcons.group_solid,
+                    color: Colors.white,
+                    size: 28,
+                  ),
                 ),
-                child: Icon(
-                  CupertinoIcons.group_solid,
-                  color: Colors.white,
-                  size: 28,
-                ),
-              ),
-              Text(
-                "HamzaCH",
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ),
-          Text(
-            "Virus",
-            style: GoogleFonts.poppins(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: Colors.white,
-              decoration: TextDecoration.underline,
-              decorationColor: Colors.white,
+                Text(
+                  authorName,
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                )
+              ],
             ),
-          ),
-          SizedBox(
-            width: boxSize.width / 1.5,
-            child: Text(
-              "How Corona virus is going to impact you next trip",
+            Text(
+              title,
               style: GoogleFonts.poppins(
-                fontSize: 16,
+                fontSize: 20,
                 fontWeight: FontWeight.w500,
                 color: Colors.white,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.white,
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              width: boxSize.width / 1.5,
+              child: Text(
+                subtitle,
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+    ]);
   }
 }
